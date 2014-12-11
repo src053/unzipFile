e94068b43bf8cf57fb4c1ff18325e849013d068b
src053 <src053@gmail.com>
2014-12-10 22:09:58 -0800

    add import

---
+++ unzipFile.py
@@ -0,0 +1,4 @@
+# This script will take look for any .7zip files in a specified location (remoter or local) and copy to a specified directory 
+# then unzip the file for a specified amount of time before cleaning it out
+
+import subprocess
