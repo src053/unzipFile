35991d47105bb5965a790a9bb7fa881e9fdf0014
src053 <src053@gmail.com>
2015-01-19 21:09:55 -0800

    multiple adds

--- unzipFile.py
+++ unzipFile.py
@@ -17,6 +17,42 @@ class Folder:
 	def __init__(self, stat):
 		self.creationTime =  time.ctime(stat.st_ctime)#set the path variable to the provided string
 		#self.createTime = creationTime #get the ctime of the folder
+def checkFolderExist(path):
+	pathToCheck = os.path.join(os.getcwd, path)
+
+	if os.path.exists(pathToCheck):
+		directoryName = path
+
+def createHostNameFolder(folderPath, z7Name):
+	"""this function will create the hostname directory for source 7z file to be copied too"""
+	#create a variable for temporary hostname file
+	tempHostNameDir = folderPath + "\\tempHostNameDir"
+	#check if the tempHostNameDir folder exists
+	if not os.path.exists(tempHostNameDir):
+		#Make the temporary directory
+		os.mkdir(tempHostNameDir)
+		print 'CREATED', tempHostNameDir
+		#call the unzip function to unzip the hostname.txt file to the temp HostNameDir
+		unzip(folderPath, tempHostNameDir, False)
+
+		hostFileName = tempHostNameDir + "\\hostname.txt"
+		#Read in the name of the hostname
+		with open(hostFileName, 'r') as f:
+			hostDirName = f.read()
+		#create destination folder
+		if not os.path.exists(os.path.join(folderPath, hostDirName)):
+			#call the unzip function to unzip the whole directory into the permanent folder
+			unzip(os.path.join(folderPath, z7Name), os.path.join(folderPath, hostDirName + "00"), True)
+			#remove the tempHostNameDir that was created
+			shutil.rmtree(tempHostNameDir)
+		else:
+			folderNumber = int(hostDirName[-2:])
+			if folderNumber < 10:
+				folderNumber += 1
+				changeToString = "0" + str(folderNumber)
+				hostDirName = hostDirName
+				print folderNumber
+				print type(folderNumber)
 
 def moveFile(destinationPath, srcName):
 	""" Used to move the file to a destination """
@@ -32,6 +68,7 @@ def crawl(folderPath, destinationPath, hoursOld):
 	global ONEHOUR
 	#Regular expression to find 7zip files .*\.7z$
 	extenMatch = re.compile('.*\.7z$')
+	machineName = re.compile('hostname.txt')
 
 	#create the hourse old in EPOCH time
 	hoursOld = time.time() - (ONEHOUR * hoursOld)
@@ -57,10 +94,27 @@ def crawl(folderPath, destinationPath, hoursOld):
 	    		# print 'the file is %s old' % fnameAge 
 	      		#test the regex and move 7zip file if it passes
 	      		
-	      		if extenMatch.match(fname): #another possible way to match is with simple string function fname.endswith(".py")
+	      		if extenMatch.match(fname): #another possible way to match is with simple string function fname.endswith(".7z")
 	      			moveFile(destinationPath, os.path.join(dirName, fname))
+	      			createHostNameFolder(destinationPath, fname)
+	      			#unzip(os.path.join(dirName, fname), destinationPath, True)
 	      		else:
 	      			print 'file %s is not a 7zip file' % fname
+
+def unzip(folderPath, destinationPath, hostInfo):
+	""" This function will call the 7z exe and unzip a compressed file"""
+	#create a variable with the 7z.exe path
+	exeLocation = "c:\\program files\\7-zip\\7z.exe"
+	#call the subprocess
+	#check if the machineInfo is already provided
+	if hostInfo:
+		print 'HOSTINFO =', hostInfo
+		print 'FOLDERPATH =', folderPath
+		subprocess.call(exeLocation + " x " + folderPath + " -o" + destinationPath)
+	else:
+		print 'HOSTINFO =', hostInfo
+		print 'FOLDERPATH =', folderPath
+		subprocess.call(exeLocation + " e " + folderPath + " -o" + destinationPath + " hostname.txt -r")
 
 if __name__ == "__main__":
 
@@ -105,7 +159,7 @@ if __name__ == "__main__":
 	print 'Hours back to scan :', hoursOld
 
 	#test that the input path is valid
-	if os.path.exists(folderPath):
+	if os.path.exists(folderPath) and os.path.exists(destinationPath):
 		#send the input variables to the crawl function
 		crawl(folderPath, destinationPath, hoursOld)
 	#If the source path fails the path validation then print a warning with the parser usage
